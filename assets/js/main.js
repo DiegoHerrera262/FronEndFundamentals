e69c7ffbd65bb5e0400f8688a7b78803bacbd585
diegoherrera262 <diegoherrera262@gmail.com>
2021-10-16 20:51:25 -0500

    feature: added motion to blog cards slider. Missing animation.

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,33 @@ triggerContactForm = () => {
   alert("Triggering Contact Form");
   console.log("Triggering Contact Form");
 };
+
+moveBlogSliderLeft = () => {
+  blogCards = document.getElementById("home-blog-wrapper").children;
+  if (blogCards[0].className !== "blog-card active-post") {
+    for (let idx = 0; idx < blogCards.length - 1; idx++) {
+      // swap classes of 0th and idx-th child
+      [blogCards[idx].className, blogCards[idx + 1].className] = [
+        blogCards[idx + 1].className,
+        blogCards[idx].className,
+      ];
+    }
+  }
+};
+
+moveBlogSliderRight = () => {
+  blogCards = document.getElementById("home-blog-wrapper").children;
+  // periodic exchange of card classes
+  if (
+    blogCards[blogCards.length - 1].className !==
+    "blog-card active-post third-post"
+  ) {
+    for (let idx = 0; idx < blogCards.length - 1; idx++) {
+      // swap classes of 0th and idx-th child
+      [blogCards[0].className, blogCards[idx + 1].className] = [
+        blogCards[idx + 1].className,
+        blogCards[0].className,
+      ];
+    }
+  }
+};
